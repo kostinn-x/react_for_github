15798cc67f20d21b0ffa42462de15eec713a4394
kostinn <kostinnarthur@gmail.com>
2023-07-15 23:09:01 +0800

    Commit - 1

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+
+                <p>
+                    <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur at, ea est expedita fuga id illum impedit iusto laudantium neque nostrum numquam omnis quam recusandae saepe sequi tempora vero voluptatibus.</span><span>Consectetur deserunt fuga nihil numquam provident quidem temporibus unde. Aut autem culpa cum cumque, doloremque ea esse excepturi expedita explicabo facilis modi, molestias nemo obcaecati perferendis provident, ratione tempore. Accusamus.</span><span>Atque corporis doloremque ducimus eaque enim eum facilis fugit, illo iusto laudantium magnam modi molestias nam non nulla odit omnis quae quia quidem rem reprehenderit rerum sed vel veritatis vero.</span><span>Autem consequatur, consequuntur cupiditate enim ex in incidunt itaque maiores nam natus nobis placeat quis quod saepe, veritatis vero voluptate voluptates voluptatum! Ducimus eum fuga labore modi omnis, quidem ut?</span>
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
